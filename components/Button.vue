5dabe9f5312db45c6707a365c0b7904f87d9e760
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-08-02 18:33:00 +0900

    feat: ボタンパーツの追加

---
+++ components/Button.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  bgcolor?: string;
+  color?: string;
+  width?: string | number;
+}>();
+</script>
+
+<template>
+  <button
+    :style="{ backgroundColor: props.bgcolor, color: props.color, width: props.width }"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  width: 100%;
+  padding: 8px 0;
+  border: none;
+  border-radius: 1000px;
+  background: var(--color-light-gray);
+  outline: none;
+  cursor: pointer;
+
+  appearance: none;
+}
+</style>
---
+++ components/LinkButton.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  href: string;
+  color?: string;
+  bgcolor?: string;
+  width?: string | number;
+}>();
+</script>
+
+<template>
+  <a
+    :href="href"
+    :style="{ backgroundColor: props.bgcolor, color: props.color, width: props.width }"
+  >
+    <slot />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+a {
+  width: 100%;
+  padding: 8px 0;
+  border: none;
+  border-radius: 1000px;
+  background: var(--color-light-gray);
+  text-decoration: none;
+  text-align: center;
+  outline: none;
+  cursor: pointer;
+
+  appearance: none;
+}
+</style>
